f2135615f04a0a9ea04d8e968a79a2c1409719cb
OshkarVTec <a01275287@tec.mx>
2023-04-19 12:03:41 -0600

    Clase para el disparo

---
+++ Shot.cpp
@@ -0,0 +1,104 @@
+#include "Shot.h"
+#include "math.h"
+
+Shot::Shot(int dim, float vel,float x, float y, float z, float height)
+{
+    DimBoard = dim;
+    //srand(time(nullptr));
+    int c;
+
+    //Se inicializa una posicion aleatoria dentro del tablero
+    Position[0] = x;
+    Position[1] = y;
+    Position[2] = z;
+
+    //Se inicializa el vector de direccion con un valor aleatorio
+    Direction[0] = rand();
+    Direction[1] = rand();
+    Direction[2] = rand();
+
+    //Se normaliza el vector de direccion
+    float m = sqrt(Direction[0]*Direction[0] + Direction[1]*Direction[1] + Direction[2]*Direction[2]);
+    Direction[0] /= m;
+    Direction[1] /= m;
+    Direction[2] /= m;
+    //se multiplica el vector de direccion por la magnitud de la velocidad
+    Direction[0] *= vel;
+    Direction[1] *= vel;
+    Direction[2] *= vel;
+
+    //cal. del radio Shot
+    this->height = height;
+    radio = height / 2;
+    cout << Direction[0] << endl;
+    cout << Direction[2] << endl; 
+    collision = false;
+}
+
+Shot::~Shot()
+{
+    //dtor
+}
+
+void Shot::draw()
+{
+    glPushMatrix();
+    glTranslatef(Position[0],Position[1], Position[2]); 
+    glRotatef(90.0f, 0.0f, 0.0f, 1.0f); 
+    glColor3f(0.0f, 1.0f, 0.0f);
+    GLUquadricObj* quad = gluNewQuadric();
+    gluQuadricNormals(quad, GLU_SMOOTH);
+    gluCylinder(quad, 5, 5, height, 30, 30);
+
+    // Draw top disk
+    gluQuadricOrientation(quad, GLU_INSIDE);
+    gluDisk(quad, 0.0, 5, 30, 1);
+    gluQuadricOrientation(quad, GLU_OUTSIDE);
+
+    // Draw bottom disk
+    glTranslatef(0.0, 0.0, height);
+    gluQuadricOrientation(quad, GLU_INSIDE);
+    gluDisk(quad, 0.0, 5, 30, 1);
+    gluQuadricOrientation(quad, GLU_OUTSIDE);
+
+    gluDeleteQuadric(quad);
+    glPopMatrix();
+}
+
+void Shot::update()
+{
+        if(!collision){
+         float new_x = Position[0] + Direction[0];
+         float new_z = Position[2] + Direction[2];
+
+         //cout << "X=" << Position[0] << "; Y= " << Position[2] << endl;
+
+         if (abs(new_x) <= DimBoard)
+               Position[0] = new_x;
+         else {
+               Direction[0] *= -1.0;
+               Position[0] += Direction[0];
+         }
+
+         if (abs(new_z) <= DimBoard)
+               Position[2] = new_z;
+         else {
+               Direction[2] *= -1.0;
+               Position[2] += Direction[2];
+         }
+
+        //cout << "X=" << Position[0] << "; Y= " << Position[2] << endl;
+    }
+}
+
+float Shot::getRadio(){
+   return radio;
+}
+
+tuple<float, float, float> Shot::getPos(){
+   return make_tuple(Position[0], Position[1], Position[2]);
+}
+
+void Shot::collided(){
+   collision = true;
+}
---
+++ Shot.h
@@ -0,0 +1,37 @@
+#ifndef SHOT_H
+#define SHOT_H
+
+#include <math.h>
+#include <random>
+#include <iomanip>
+#include <GL/glut.h>
+#include <iostream>
+#include <tuple>
+
+
+using namespace std;
+
+class Shot
+{
+    public:
+        Shot(int, float, float, float, float, float);
+        ~Shot();
+        void draw();
+        void update();
+        tuple<float,float,float> getPos();
+        float getRadio();
+        void collided();
+
+    protected:
+
+    private:
+        //Vector de direccion
+        float Position[3];
+        float Direction[3] = {0.0, 5.0, 0.0};
+        int DimBoard;
+        float radio;
+        bool collision;
+        float height;
+};
+
+#endif // Shot_H
